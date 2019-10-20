45cffd18dc09e5cf22fea8e7741db2beb4127fed
ashish <ashish@skytesters.com>
2019-10-20 18:37:29 +0530

    a

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from './Logo';
+const Header = () =>{
+    return(
+        <>
+            <header className="header">
+                <div className="header__container">
+                        this is headaer
+                        <Logo  alt="aaaaa"/>
+                </div>
+
+            </header>
+        </>
+    )
+
+
+}
+export default Header;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Logo = (props) =>{
+    return(
+        <>
+            <img src="https://www.udemy.com/staticx/udemy/images/v6/logo-coral.svg" alt={props.alt} />
+        </>
+    )
+
+
+}
+export default Logo;
