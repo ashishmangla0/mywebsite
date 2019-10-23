7e08e0ead8d3af68ba8b2f038a80ae006e8608f1
ashish <ashish@skytesters.com>
2019-10-23 23:51:28 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import React from 'react';
+import React,{useState,useCallback} from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Container from './components/index';
+
 import Header from './components/Header';
-import Container from './components/container';
 import NavbarBrand from './components/navbarBrand';
 import Logo from './components/Logo';
+
 function App() {
+  
+  const [showText, setShowText] = useState(false);
+  const aa = () =>{
+    setShowText(!showText);
+  }
+
   return (
     <>
-<Header>
-    <Container>
-      <NavbarBrand href="/" className="Header__navbar" >
-        <Logo text="udemy" logoLink="logo1.png" className="header__Logo"/>
-      </NavbarBrand>
-    </Container>
-
-</Header>
-<section>
-  <Container>
-    this is container
+      <Header>
+        <Container>
+          <NavbarBrand href="/" className="Header__navbar" >
+            <Logo text="udemy" logoLink="images/logo.png" className="header__Logo" />
+          </NavbarBrand>
+          <nav className="ml-auto">
+              <ul className="navbar-nav">
+                <li>
+                  <a>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a>Services
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Products
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Industries
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Career
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <span onClick={aa}>
+              clickme
+            </span>
+        </Container>
+      </Header>
+      <section>
+        <Container>
+          this is container <p> 
+    {showText && <div>This text will show!</div>}</p>
   </Container>
-</section>
-    
-    </> 
+      </section>
+      
+    </>
   );
 }
 export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+import Header from './Header';
+import Container from './container';
+import NavbarBrand from './navbarBrand';
+import Logo from './Logo';
+
+export default Container;
+// export const Header;
